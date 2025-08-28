9094ba3d844a04016c16fc6fcf6d41a2f03d5873
Kauã <kauafragozo18@gmail.com>
2025-08-27 22:29:58 -0300

    Estruturação da V2

--- v2-CSharp/IMCManager/Program.cs
+++ v2-CSharp/IMCManager/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using IMCManager.Models;
+
+
+namespace IMCManager
+{
+    internal class Program
+    {
+
+        }
+
+
+    }
+
---
+++ v2-CSharp/IMCManager/models/Pacientes.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+
+namespace IMCManager.Models
+{
+    public class Pacientes
+    {
+        public string Nome { get; set; }
+        public string Classe { get; set; }
+        public string Objetivo { get; set; }
+        public double Peso { get; set; }
+        public double Altura { get; set; }
+        public double IMC { get; set; }
+
+
+
+
+
+    }
+}
